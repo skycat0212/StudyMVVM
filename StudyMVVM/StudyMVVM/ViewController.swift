8821687ef84c3cc7e08206d463a8b9eb9ace5371
skycat0212 <mrmagnet777@gmail.com>
2022-09-29 16:54:16 +0900

    프로젝트 세팅

---
+++ StudyMVVM/StudyMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyMVVM
+//
+//  Created by 김수진 on 2022/09/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
